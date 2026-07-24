3c439bd16d14cfdb431f26a09ec01a5296e9adf1
Meet Pipaliya <24010101220@darshan.ac.in>
2026-07-24 21:10:55 +0530

    Time: 1 ms (96.4%), Space: 43.8 MB (79.12%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> li = new ArrayList<>();
+
+        int max = 0;
+        for(int i = 0; i < candies.length; i++) max = Math.max(max, candies[i]);
+
+        for(int i = 0; i < candies.length; i++) {
+            if((candies[i] + extraCandies) >= max)li.add(true);
+            else li.add(false);
+        }
+
+        return li;
+    }
+}
