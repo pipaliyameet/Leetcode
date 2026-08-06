de1cae86d4e91116c125c687df7bf7ef0aa4f6d5
Meet Pipaliya <24010101220@darshan.ac.in>
2026-08-06 19:12:34 +0530

    Time: 1 ms (100%), Space: 42.5 MB (66.49%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+
+        int num = n;
+
+        while(true){
+            int temp = num;
+            int multy = 1;
+
+            while(temp > 0){
+                multy *= temp % 10;
+                temp /= 10;
+            }
+
+            if(multy % t == 0) return num;
+
+            num++;
+        }
+    }
+}
