3156aa9eca7f5e78e3aa6076e2aa9c06ae1a0c19
Meet Pipaliya <24010101220@darshan.ac.in>
2026-09-07 19:34:18 +0530

    Time: 347 ms (94.1%), Space: 0B (100%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select product_name, sum(unit) unit
+from Products p 
+join Orders o
+on p.product_id = o.product_id
+where year(order_date) = 2020 and month(order_date) = 2
+group by p.product_name
+having sum(unit) > 99
